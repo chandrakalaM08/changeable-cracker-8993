5fe212fdf532f9e9ab85c838c662879d69c8463d
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-06-15 22:27:31 +0530

    day-3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             color={useColorModeValue('gray.700', 'gray.200')}>
             <Container as={Stack} maxW={'8xl'} py={10}>
                 <SimpleGrid
-                    templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
+                templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
                     spacing={8}>
                     <Stack spacing={6}>
                         <Box>
