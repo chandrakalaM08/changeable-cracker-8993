1bef081aba8dd66fcce3070545b20950a7030242
Sonika2223Kanojia <sonikakanojia534@gmail.com>
2023-06-16 12:00:16 +0530

    day3

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,7 +13,7 @@ import {
     useColorModeValue,
   } from '@chakra-ui/react';
   
-  export default function SimpleCard() {
+  export default function Login() {
     return (
       <Flex
         minH={'100vh'}
