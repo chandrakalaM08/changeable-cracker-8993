98da92764d02e007c18f55e725c383ff1218624c
chandrakalaM08 <chandrakala.2508@gmail.com>
2023-06-18 18:28:22 +0530

    day 6 changes

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import TopSectionPicture from "../assets/TopSectionImg.png"
+import { useNavigate } from 'react-router'
+import './TopSection.css'
 const TopSection = () => {
+
+    const navigate = useNavigate()
+
+    function onClickOnViewCollection() {
+        navigate("/products")
+    }
     return (
         <div>
-
+            <div className='collection'>
+                <div style={{ marginTop: '60px' }}>
+                    <img
+                        src='https://static.zara.net/photos///contents/mkt/spots/ss23-north-woman-shoes-bags/subhome-xmedia-24-2//w/1920/IMAGE-landscape-default-fill-48cbf8b6-5b24-4620-b212-3a620d493bb3-default_0.jpg?ts=1686757325120'
+                        alt='image1' />
+                </div>
+                <div>
+                    <img
+                        src='https://static.zara.net/photos///contents/mkt/spots/ss23-north-woman-join-life/subhome-xmedia-24//w/1920/IMAGE-landscape-fill-a2628e19-7996-4884-9a5e-2d33ab4bcb45-default_0.jpg?ts=1686664293290'
+                        alt='image2' />
+                    <button onClick={onClickOnViewCollection} className="border-gradient border-gradient-purple">View All Collections</button></div>
+                <div style={{ marginTop: '40px' }}>
+                    <img
+                        src='https://static.zara.net/photos///contents/mkt/spots/ss23-north-woman-knitwear/subhome-xmedia-24-2//w/1920/IMAGE-landscape-default-fill-56c22222-74cf-436b-ae43-48adf9e9d5c2-default_0.jpg?ts=1686817957817'
+                        alt='image3' /></div>
+            </div>
         </div>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,78 @@
 import React from 'react'
 import CarouselCard from '../components/CarouselCard'
 import TopSection from '../components/TopSection'
+import { Box } from '@chakra-ui/layout'
+import { Image } from "@chakra-ui/react";
+import homepagephoto from "../assets/hmgoepprod-removebg-preview.jpg"
+import cover from "../assets/cover.jpg"
+import boystanding from "../assets/boystanding-removebg-preview.jpg"
+import boysitting from "../assets/boysitting.jpg"
 
 const HomePage = () => {
   return (
-      <div>
+    <div >
+        <Image
+      src={cover}
+      alt="girl"
+      boxSize={"100%"}
+      objectFit="cover"
+    />
+      <br/>
+       <Box
+        display="flex"
+      flexDirection={{ base: "column", md: "row" }}
+        justifyContent="space-around"
+      
+    >
+      <Box
+        width={{ base: "100%", md: "30%" }}
+        height="200px"
+        mb={{ base: "20px", md: "0" }}
+        >
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>Fashion is a form of self
+                <br/>
+            -expression and autonomy
+            <br />
+            at a particular period.
+          </p>
+          <br />
+          <br />
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>READ MORE 🔗</p>
+      </Box>
+      <Box
+        width={{ base: "100%", md: "30%" }}
+        height="200px"
+        mb={{ base: "20px", md: "0" }}
+        >
+        <p style={{ fontSize: "45px", fontWeight: "600" }}> CLOTHS ARE    <br /> THE SPIRIT   <br /> OF FASHION</p> 
+          </Box>
+      {/* <Box
+        width={{ base: "100%", md: "30%" }}
+        height="200px"
+        >
+ <Image
+      src={homepagephoto}
+      alt="girl"
+            width={{ base: "50%", md: "50%", lg: "50%" }}
+            height={270}
+    />          </Box> */}
+         <Box
+        width={{ base: "100%", md: "30%" }}
+        height="200px"
+        mb={{ base: "20px", md: "0" }}
+        >
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>Fashion is the part of <br />the daily air and it changes <br />
+            all the time, 
+          with all the events. 
+          </p>
+          <br />
+          <br />
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>READ MORE 🔗</p>
+      </Box>
+      </Box>
+     
+      <br />
+      <br/>
       <TopSection/>
              <p
                 style={{
@@ -19,7 +87,82 @@ const HomePage = () => {
             >
                 POPULAR PRODUCTS
             </p>
-          <CarouselCard />
+      <CarouselCard />
+      
+       <br />
+       <Box
+        display="flex"
+      flexDirection={{ base: "column", md: "row" }}
+        justifyContent="space-around"
+     
+    >
+       <Box  display="flex"
+      justifyContent="center"
+        width={{ base: "100%", md: "60%" }}
+        >
+ <Image
+      src={boystanding}
+            alt="girl"
+            width={{ base: "100%", md: "60%", lg: "50%" }}
+            height={450}
+    />          </Box>
+      <Box
+        width={{ base: "100%", md: "30%" }}
+        height="200px"
+          mb={{ base: "20px", md: "0" }}
+          textAlign={"left"}
+        >
+          <p style={{ fontSize: "35px", fontWeight: "600" ,textAlign:"center"}}> 25% OFF  <br />
+            <span style={{ fontSize: "20px", fontWeight: "600" }}>on our all new arrivals</span>   <br /> OF FASHION</p> 
+          <br />
+          <br />
+          <p style={{ fontSize: "25px", fontWeight: "600" ,textAlign:"center"}}> Outfit Inspiration <br />
+           Snag their style..</p> 
+          </Box>
+        <Box
+        width={{ base: "100%", md: "30%" }}
+          height="200px"
+          marginRight={20}
+          textAlign={"right"}
+        mb={{ base: "20px", md: "0" }}
+        >
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>Fashion is a form of self
+                <br/>
+            -expression and autonomy
+            <br />
+            at a particular period.
+          </p>
+          <br />
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>In order to be irreplaceable,
+            <br/>one must always be different.</p>
+          <br />
+          <br />
+           <p style={{ fontSize: "15px", fontWeight: "600" }}>
+            Fashion is the most powerful art <br />
+            there is. It's movement, design, and
+            <br />architecture all in one.
+            <br />
+            It shows the world
+            <br />who we are
+            <br/>and who we'd like to be.</p>
+          <br />
+          <p style={{ fontSize: "15px", fontWeight: "600" }}>READ MORE 🔗</p>
+          <br />
+          
+        </Box>
+        <Box
+          width={{ base: "100%", md: "60%" }}
+           display="flex"
+      justifyContent="center"
+        >
+ <Image
+      src={boysitting}
+            alt="girl"
+            width={{ base: "100%", md: "50%", lg: "50%" }}
+            height={450}
+    />          </Box>
+      </Box>
+      <br/>
              <p
                 style={{
                     textAlign: "left",
