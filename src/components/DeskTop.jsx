f6416b0b6cd04da2d9e68b7f5f673bad038fcfe5
chandrakalaM08 <chandrakala.2508@gmail.com>
2023-06-19 00:17:00 +0530

    final changes

--- src/components/DeskTop.jsx
+++ src/components/DeskTop.jsx
@@ -3,18 +3,59 @@ import { useNavigate } from "react-router-dom"
 import { AiFillHeart, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 import "./DeskTop.css";
 import { Button } from "@chakra-ui/button";
-export const DeskTop = () => {
+import { useSelector } from "react-redux";
+import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
+import { signout } from "../redux/AuthReducer/action";
+import { useDispatch } from "react-redux";
 
+
+export const DeskTop = () => {
+    let { isAuth } = useSelector((store) => store.AuthReducer)
     const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+    const userEmail = localStorage.getItem("loggedInUser")
+    const userInfo = JSON.parse(localStorage.getItem("users") || [])
+
+    const getActiveUserName = () => {
+        let activeUser = "JD"
+        console.log(userEmail)
+        console.log(userInfo)
+        for (let i = 0; i < userInfo.length; i++) {
+            console.log(userInfo[i].userEmail)
+            if (userInfo[i][userEmail] !== undefined) {
+                activeUser = userInfo[i][userEmail]
+            }
+        }
+        return activeUser
+    }
+
+    console.log(getActiveUserName(), " is the active user")
 
     const handleCartClick = () => {
         navigate("/cart")
     };
 
     const handleLoginClick = () => {
-        navigate("/login")
+        if (isAuth === false) {
+            navigate("/login")
+        }
     };
 
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            navigate("/");
+            console.log("Signed out successfully")
+            dispatch(signout)
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
+
+    console.log(isAuth)
 
     return <>
         <div style={{ display: "flex", marginLeft: "70px", justifyContent: "space-around", background: "white", width: "100%" }}>
@@ -43,7 +84,23 @@ export const DeskTop = () => {
                 onClick={handleCartClick}>
                 Cart <AiOutlineShoppingCart />
             </Button>
-            <Button
+            {isAuth ? <><button style={{ fontWeight: "600" }}>{getActiveUserName()}</button><Button
+                onClick={handleLogout}
+                as={'a'}
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'md'}
+                fontWeight={600}
+                color={'white'}
+                bg={'pink.500'}
+                href={'#'}
+                _hover={{
+                    bg: 'pink.200',
+                }}>
+                Sign Out
+            </Button>
+            </>
+                :
+                <Button
                 onClick={handleLoginClick}
                 as={'a'}
                 display={{ base: 'none', md: 'inline-flex' }}
@@ -55,8 +112,8 @@ export const DeskTop = () => {
                 _hover={{
                     bg: 'pink.200',
                 }}>
-                Sign In
-            </Button>
+                    Sign In 
+                </Button>}
         </div>
     </>
 } 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,23 +9,139 @@ import {
     Link,
     Button,
     Heading,
-    Text,
-    useColorModeValue,
+  Text,
+    Image,
+  useColorModeValue,
+    useToast
   } from '@chakra-ui/react';
   
-  export default function LoginPage() {
+
+  import loginImg from "../assets/login.png"
+import { NavLink } from 'react-router-dom';
+
+import { LoginType } from '../redux/types';
+import { useNavigate,useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
+import { login } from '../redux/AuthReducer/action';
+import { useEffect, useRef, useState } from 'react';
+ 
+
+export default function LoginPage() {
+    const navigate = useNavigate()
+  const dispatch: Dispatch<any> = useDispatch()
+  
+    const location = useLocation()
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const toast = useToast()
+  const toastIdRef = useRef<string | number | undefined>();
+  const {loginToken, isAuth, errorMsg} = useSelector((store:any)=>store.AuthReducer)
+  const [loginData, setLoginData] = useState<LoginType>({
+    email : '', 
+    password : ''
+  })
+
+  const [user , setUser] = useState<string>("")
+
+  const handleLogin = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!loginData.email || !loginData.password) {
+      return toast({
+        title: 'Failed!!',
+        description: "Please fill all input fields.",
+        status: 'error',
+        position: 'top',
+        duration: 4000,
+        isClosable: true,
+      })
+    }
+
+    dispatch(login(loginData))
+    // localStorage.setItem("user", JSON.stringify())
+  }
+
+  useEffect(() => {
+  
+    if(loginToken){
+      toastIdRef.current = toast({
+        title: 'Welcome back👍',
+        description: 'User logged in successfully',
+        status: 'success',
+        position: 'top',
+        duration: 4000,
+        isClosable: true,
+      }) as string;
+      localStorage.setItem("loggedInUser", loginData.email)
+       setTimeout(()=>{
+         navigate(location.state, { replace: true })
+       },500)
+    }
+  
+    
+    return () => {
+     
+      if (toastIdRef.current) {
+        toast.close(toastIdRef.current);
+      }
+    };
+  }, [loginToken])
+
+  useEffect(()=>{
+    if(errorMsg === "Firebase: Error (auth/wrong-password)."){
+      toastIdRef.current = toast({
+        title: 'Login Failed!!',
+        description: 'Wrong Password, Please try again.',
+        status: 'error',
+        position: 'top',
+        duration: 4000,
+        isClosable: true,
+      }) as string;
+    }
+    return () => {
+     
+      if (toastIdRef.current) {
+        toast.close(toastIdRef.current);
+      }
+    };
+   },[errorMsg,toast])
+
+
+  useEffect(()=>{
+    if(errorMsg === "Firebase: Error (auth/user-not-found)."){
+      toastIdRef.current = toast({
+        title: 'Login Failed!!',
+        description: 'User Not found! Please Signup First',
+        status: 'error',
+        position: 'top',
+        duration: 4000,
+        isClosable: true,
+      }) as string;
+
+      setTimeout(() => {
+        navigate('/signup');
+      }, 1000)
+    }
+    return () => {
+     
+      if (toastIdRef.current) {
+        toast.close(toastIdRef.current);
+      }
+    };
+   },[errorMsg])
+  
     return (
       <Flex
-        minH={'100vh'}
+        minH={'45vh'}
         align={'center'}
         justify={'center'}
         bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
+       
+          <Image src={loginImg} width={ "38%"} />
+       
+        <Stack spacing={8} mx={'auto'} maxW={'xl'} py={12} px={6}>
+          <Stack align={'left'}>
             <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-            </Text>
           </Stack>
           <Box
             rounded={'lg'}
@@ -33,34 +149,49 @@ import {
             boxShadow={'lg'}
             p={8}>
             <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: 'column', sm: 'row' }}
-                  align={'start'}
-                  justify={'space-between'}>
-                  <Checkbox>Remember me</Checkbox>
-                  <Link color={'blue.400'}>Forgot password?</Link>
+              <form onSubmit={handleLogin}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
+                  <Input value={loginData.email} onChange={(e) => setLoginData(prev => ({...prev, email : e.target.value}))} borderRadius={'none'} type="email" placeholder='Enter your email' />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input value={loginData.password} onChange={(e) => setLoginData(prev => ({...prev, password : e.target.value}))} borderRadius={'none'} type="password" placeholder='Enter your password' />
+                </FormControl>
+                <Stack spacing={10}>
+                  <Stack
+                    m={'30px 0 -10px 0'}
+                    direction={{ base: 'column', sm: 'row' }}
+                    align={'start'}
+                    justify={'space-between'}>
+                    <Heading color={'gray.600'} fontWeight={'600'} size={'sm'}>New here?</Heading>
+                    <Link href={'/signup'} style={{ fontWeight: '600', color: '#499de2' }}>Signup</Link>
+                  </Stack>
+                  <Button
+                    borderRadius={'5px'}
+                    type='submit'
+                    bg={'pink.500'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'orange',
+                    }}>
+                    Sign in
+                  </Button>
                 </Stack>
-                <Button
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Sign in
-                </Button>
-              </Stack>
+              </form>
             </Stack>
+            <Stack  p={5}
+                    align={'center'}
+                style={{paddingTop:"40px"}}>
+                  <Text align={'center'}>
+                    
+                    New to Clothy?
+                    <NavLink to="/signup"><Link color={'orange.800'} style={{paddingLeft:"10px"}}>Create Account</Link></NavLink>
+                  </Text>
+                </Stack>
           </Box>
         </Stack>
       </Flex>
     );
   }
+
